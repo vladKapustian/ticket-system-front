e74d8a303f500deda18575c328c8c83f81e702ea
Vladislav Kapustyan <vladislav.kapustyan@immelman.ru>
2023-06-08 02:16:37 +0300

    Господи помоги

--- src/pages/request/index.tsx
+++ src/pages/request/index.tsx
@@ -4,9 +4,16 @@ import Image from "next/image";
 import ReportImage from "../../../public/reportImage.svg";
 import styles from "./styles.module.scss";
 import { Button, Checkbox, Form, FormInstance, Input, Select } from "antd";
+import { Typography } from "antd";
 
 const { Option } = Select;
 const { TextArea } = Input;
+const { Title, Paragraph } = Typography;
+interface IRequestFormValue {
+  reporterName: string;
+  reporterEmail: string;
+  title: string;
+}
 
 export default function Request() {
   const formRef = React.useRef<FormInstance>(null);
@@ -21,87 +28,89 @@ export default function Request() {
         break;
       case "HIGH":
         formRef.current?.setFieldsValue(value);
+        break;
     }
   };
-  const onFormSubmit = () => {};
+  const onFormSubmit = (values: IRequestFormValue) => {};
   return (
     <div className={styles.requestPageContainer}>
       <Image className={styles.reportImage} src={ReportImage} alt="" />
       <div className={styles.requestFormContainer}>
-        <h1 className={styles.formPageTitle}>Требуется помощь?</h1>
-        <p className={styles.formPageDescriptionText}>
+        <Title className={styles.formPageTitle}>Требуется помощь?</Title>
+        <Paragraph className={styles.formPageDescriptionText}>
           Мы очень ценим ваше мнение и готовы оказать вам помощь с любой проблемой при использовании нашими продуктами.
           Пожалуйста, оставьте информацию о проблеме, а также свои комментарии контактные данные, чтобы мы могли вам
           оказать поощь как можно быстрее!
-        </p>
+        </Paragraph>
         <Form
           layout="vertical"
-          size="large"
+          size="middle"
           name="normal_login"
           initialValues={{ remember: true }}
           onFinish={onFormSubmit}
         >
           <Form.Item
+            className={`${styles.formItem}`}
             label="Как к вам можно обращаться"
             name="reporterName"
-            rules={[{ required: true, message: "Поле 'Как к вам можно обращаться' является обязательным" }]}
+            rules={[{ required: true, message: "Поле обязательно для заполнения" }]}
           >
             <Input placeholder="Имя" />
           </Form.Item>
           <Form.Item
+            className={`${styles.formItem}`}
             label="Email"
             name="reporterEmail"
-            rules={[{ required: true, message: "Поле 'Email' является обязательным" }]}
+            rules={[{ required: true, message: "Поле обязательно для заполнения" }]}
           >
             <Input placeholder="Фамилия" />
           </Form.Item>
           <Form.Item
+            className={`${styles.formItem}`}
             label="Тема сообщения"
             name="title"
-            rules={[
-              { required: true, message: "Поле 'Тема сообщения' является обязательным" },
-              {
-                required: true,
-                type: "email",
-                message: "Пожалуйста, введите корректную почту",
-              },
-            ]}
+            rules={[{ required: true, message: "Поле обязательно для заполнения" }]}
           >
             <Input placeholder="Логин" />
           </Form.Item>
           <Form.Item
+            className={`${styles.formItem}`}
             label="Уровень важности"
             name="priority"
-            rules={[{ required: true, message: "Поле 'Уровень важности' является обязательным" }]}
+            rules={[{ required: true, message: "Поле обязательно для заполнения" }]}
           >
-            <Select placeholder="Select a option and change input text above" onChange={onGenderChange} allowClear>
+            <Select placeholder="Выберите уровень важности заявки" onChange={onGenderChange} allowClear>
               <Option value="LOW">Низкая</Option>
               <Option value="MEDIUM">Средняя</Option>
               <Option value="HIGH">Высокая</Option>
             </Select>
           </Form.Item>
           <Form.Item
-            rules={[{ required: true, message: "Поле 'Текст сообщения' является обязательным" }]}
+            className={`${styles.formItem}`}
+            rules={[{ required: true, message: "Поле обязательно для заполнения" }]}
             label="Текст сообщения"
             name="description"
           >
             <TextArea
-              placeholder="Введите текст сооьщения (не более 1000 сивмолов)"
+              placeholder="Введите текст сообщения (не более 1000 сивмолов)"
               maxLength={1000}
               rows={6}
             ></TextArea>
           </Form.Item>
           <Form.Item
+            className={`${styles.formItem}`}
             name="agrrement"
             valuePropName="checked"
             rules={[
               {
                 required: true,
-                message: "Для пользованием данных нужно дать согласие на обработку персональных данных",
+                message: "Нужно дать согласие на обработку персональных данных",
               },
             ]}
           >
-            <Checkbox>Я даю согласие на обработку своих персональных данных</Checkbox>
+            <Checkbox className={`${styles.formItem} ${styles.requestFormCheckbox}`}>
+              Я даю согласие на обработку своих персональных данных
+            </Checkbox>
           </Form.Item>
 
           <Form.Item className={styles.signInButtons}>
