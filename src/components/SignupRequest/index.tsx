32a5952858e015f4f1ee1819da64a88562aee575
Vladislav Kapustyan <vladislav.kapustyan@immelman.ru>
2023-06-14 01:48:32 +0300

    Fixed signup request state components

--- src/components/SignupRequest/index.tsx
+++ src/components/SignupRequest/index.tsx
@@ -3,15 +3,18 @@ import { Button, Card, Typography } from "antd";
 import { type } from "os";
 import styles from "./styles.module.scss";
 import { api } from "@/api";
+import { useState } from "react";
 
 type Props = {
   request: TUser;
 };
 
 const SignupRequest = ({ request }: Props) => {
+  const [isSigidUp, setIsSignedUp] = useState(request.role === UserRole.OPERATOR);
   const approveSignup = () => {
     try {
       api.confirmUsers({ id: request.id });
+      setIsSignedUp(true);
     } catch (error) {
       console.error(error);
     }
@@ -26,7 +29,7 @@ const SignupRequest = ({ request }: Props) => {
         <Typography.Text>Имя: {request.firstname}</Typography.Text>
         <Typography.Text>Фамилия: {request.lastname}</Typography.Text>
 
-        {request.role === UserRole.UNCONFIRMED ? (
+        {!isSigidUp ? (
           <div className={styles.approveButtonContainer}>
             <Button type="primary" onClick={approveSignup}>
               Одобрить регистрацию
